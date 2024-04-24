7e76d6002b147942968e985b759b938aef755c2d
Buddha <domenicocrobon@gmail.com>
2024-04-24 22:03:16 +0200

    raleigh scattering

--- src/cpp/sky/sky_physics.cpp
+++ src/cpp/sky/sky_physics.cpp
@@ -1,14 +1,25 @@
 #include "sky_physics.h"
 #include "cmath"
+#include "glm/vec3.hpp"
+#include "glm/gtx/vector_angle.inl"
 
-double Sky::Density(double height) const {
-    return SeaLevelMolecularDensity * exp(-height / AtmosphericThickness);
+double sky::density(double height) const {
+    return sea_level_molecular_density * exp(-height / atmospheric_thickness);
 }
 
-double Sky::Rayleigh(double height, double wavelength) const{
-
+double sky::raleigh_scattering_coefficient(const double height) const{
     return
-    (8 * pow(M_PI, 2) * pow((pow(RefractionIndex, 2) - 1),2)) /
-    (3 * SeaLevelMolecularDensity) *
-    exp(-height/ScaleHeight);
+    (8 * pow(M_PI, 2) * pow((pow(refraction_index, 2) - 1),2)) /
+    (3 * sea_level_molecular_density) *
+    exp(-height/scale_height);
 }
+
+double sky::raleigh_phase_function(const glm::vec3 light_direction, const glm::vec3 view_direction) {
+    const double my = cos(glm::angle(light_direction, view_direction));
+    return raleigh_phase_function(my);
+}
+
+double sky::raleigh_phase_function(const double µ) {
+    return 3/16*M_PI(1+pow(µ,2));
+}
+
--- src/cpp/sky/sky_physics.h
+++ src/cpp/sky/sky_physics.h
@@ -1,23 +1,46 @@
 #ifndef MAIN_SKY_PHYSICS_H
 #define MAIN_SKY_PHYSICS_H
+#include <map>
+#include "glm/vec3.hpp"
 
-class Sky {
+class sky {
 public:
     /// In kilometers. https://en.wikipedia.org/wiki/Earth
-    double EarthRadius = 6371.0;
+    double earth_radius = 6371.0;
     /// In kilometers. https://en.wikipedia.org/wiki/Atmosphere_of_Earth
-    double atmosphereRadius = EarthRadius * 1.0157;
-    double AtmosphericThickness = 1.0;
-    double SeaLevelMolecularDensity = 1.0;
-    /// In kilometers. Paper's default is 8.
-    double ScaleHeight = 8.0;
-    int RefractionIndex = 1;
+    double atmosphere_radius = earth_radius * 1.0157;
+    double atmospheric_thickness = 1.0;
+    double sea_level_molecular_density = 1.0;
+    /// "A general way to describe how a value fades away". In kilometers.
+    double scale_height = 8.0;
+    int refraction_index = 1;
 
-    /// Calculates density at a given height.
-    double Density(double height) const;
+    /// @param height Height above sea level.
+    /// @return TODO
+    double density(double height) const;
+
     /// Rayleigh β-scattering coefficient.
-    double Rayleigh(double height, double wavelength) const;
+    /// @param height Height above sea level.
+    /// @return Scattering coefficient (β).
+    double raleigh_scattering_coefficient(double height) const;
 
+    /// 
+    /// @param light_direction Incoming light direction.
+    /// @param view_direction View direction from camera.
+    /// @return Raleigh phase value.
+    static double sky::raleigh_phase_function(glm::vec3 light_direction, glm::vec3 view_direction);
+
+    /// 
+    /// @param µ Cosine between light and view directions.
+    /// @return Raleigh phase value.
+    static double sky::raleigh_phase_function(double µ);
+
+    /// Precomputed Rayleigh coefficient values and their respective wavelengths. Keys in nanometers.
+    std::map<double, double> scatter_coefficients = {
+        {440, 33.1e-6}, // Blue
+        {550, 13.5e-6}, // Green
+        {680, 5.8e-6}   // Red
+    };
 
 }; // sky
 
